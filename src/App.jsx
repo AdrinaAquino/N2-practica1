1c2bfb7bda70ba1fc24723c0743ec079ac8d7b7a
adrianasheila <adrisheiaquiblan@gmail.com>
2025-04-04 10:55:21 -0400

    cards cambiadas

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,19 @@ export default function App() {
         <div className="w-full flex flex-col items-center md:w-3/5 md:text-white">
           <div className="w-full pb-5 flex flex-col md:flex-row md:justify-between md:bg-[#111827] ">
             <p className=" pl-5 text-xl font-bold font-serif pt-8 pb-5 md:text-3xl md:py-10 md:pl-2">The creative crew</p>
-            <p className='w-60 m-auto md:m-0 px-6 text-[10px] md:w-65 md:text-[14px]  '><strong className='text-[13px]'>Who we are</strong><br></br>We are team of creatively diverse, driven, innovative individuals working in various location from the world. </p>
+            <p className='w-60 m-auto md:m-0 px-6 text-[10px] md:w-65 md:text-[14px]  '>
+              <strong className='text-[13px]'>Who we are</strong>
+              <br></br>We are team of creatively diverse, driven, innovative individuals working in various location from the world.
+            </p>
           </div>
           <div className=' bg-white grid grid-cols-2 md:grid-cols-3 md:gap-4 md:p-4 md:bg-[#111827]'>
             <CardPersonal />
           </div>
         </div>
         <footer className='md:w-3/5'>
-          <p className='w-full text-center text-xs md:bg-[#111827] md:text-white p-2'>created by <strong>AdrianaSheila</strong>-devChallenges.io</p>
+          <p className='w-full text-center text-xs md:bg-[#111827] md:text-white p-2'>
+            created by <strong>AdrianaSheila</strong>-devChallenges.io
+          </p>
         </footer>
       </div >
     </>
--- src/components/CardPersonal.jsx
+++ src/components/CardPersonal.jsx
@@ -1,11 +1,11 @@
 export function CardPersonal() {
     const cards = [
-        { nombre: "Bill Mahoney", cargo: "PRODUCT OWNER", imagen: "./public/images/photo1.png" },
-        { nombre: "Saba Cabrera", cargo: "ART DIRECTOR", imagen: "./public/images/photo2.png" },
-        { nombre: "Shae Le", cargo: "TECHLEAD", imagen: "./public/images/photo3.png" },
-        { nombre: "Skylah Lu", cargo: "UX DESIGNER", imagen: "./public/images/photo4.png" },
-        { nombre: "Griff Richards", cargo: "DEVELOPER", imagen: "./public/images/photo5.png" },
-        { nombre: "Stan John", cargo: "DEVELOPER", imagen: "./public/images/photo6.png" },
+        { nombre: "Bill Mahoney", cargo: "PRODUCT OWNER", imagen: "./images/photo1.png" },
+        { nombre: "Saba Cabrera", cargo: "ART DIRECTOR", imagen: "./images/photo2.png" },
+        { nombre: "Shae Le", cargo: "TECHLEAD", imagen: "./images/photo3.png" },
+        { nombre: "Skylah Lu", cargo: "UX DESIGNER", imagen: "./images/photo4.png" },
+        { nombre: "Griff Richards", cargo: "DEVELOPER", imagen: "./images/photo5.png" },
+        { nombre: "Stan John", cargo: "DEVELOPER", imagen: "./images/photo6.png" },
        
     ];
 
@@ -16,7 +16,7 @@ export function CardPersonal() {
                     <div className="flex flex-col items-center even:mt-7 md:nth-[3n+2]:mt-7 " key={car} >
                         <div className='flex w-4/7'>
                             <img src={card.imagen} alt="" />
-                            <p className='h-4 rotate-90 origin-top-left mx-3 text-black text-[8px] md:text-[12px] md:text-white md:mx-5'>{card.cargo}</p>
+                            <p className=' h-4 rotate-90 origin-top-left ml-29 w-30  text-black text-[8px] md:text-[12px] md:mx-45 md:text-white  absolute'>{card.cargo}</p>
                         </div>
                         <p className='text-xs md:text-sm'>{card.nombre}</p>
                     </div>
